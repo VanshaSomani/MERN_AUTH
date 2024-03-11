89e50d0c66f8d340815e52e2546321cb533927df
VanshaSomani <vanshasomani0@gmail.com>
2024-03-11 10:54:07 +0530

    Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import { About } from './pages/About'
 import { SignIn } from './pages/SignIn'
 import { SignUp } from './pages/SignUp'
 import { Profile } from './pages/Profile'
+import { Header } from './components/Header'
 
 function App() {
   return (
     <>
     {/* <h1>APP</h1> */}
+    <Header/>
     <Routes>
       <Route path='/' element={<Home/>}></Route>
       <Route path="/about" element={<About/>}></Route>
