f5bffa0e56a24720054fb004270212d1df21de15
VanshaSomani <vanshasomani0@gmail.com>
2024-03-11 10:39:07 +0530

    Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
+// import './App.css'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { SignIn } from './pages/SignIn'
+import { SignUp } from './pages/SignUp'
+import { Profile } from './pages/Profile'
 
 function App() {
   return (
     <>
+    {/* <h1>APP</h1> */}
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path="/about" element={<About/>}></Route>
+      <Route path="/sign-in" element={<SignIn/>}></Route>
+      <Route path="/sign-up" element={<SignUp/>}></Route>
+      <Route path="/profile" element={<Profile/>}></Route>
+    </Routes>
     </>
   )
 }
