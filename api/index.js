3cc4b53a22cc06b391ecb1b8735f1cf467d072a8
VanshaSomani <vanshasomani0@gmail.com>
2024-03-11 11:32:41 +0530

    DB_Connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from "express"
+import mongoose from "mongoose";
 
 const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+mongoose.connect("mongodb://127.0.0.1:27017/MERN_AUTH").then(() => {
+    console.log("Connected to mongodb");
+}).catch((err) => {
+    console.log(err);
+})
+
 
 app.listen(3000 , () => {
     console.log("Server running on port 3000");
